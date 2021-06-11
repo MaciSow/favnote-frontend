694d8405a3213820d5bebcada1a8201c1e073c46
Maciej Sowiński <>
2021-06-11 14:02:03 +0200

    Add ButtonIcon & icons

---
+++ src/components/atoms/ButtonIcon/ButtonIcon.stories.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import { Meta } from '@storybook/react';
+import bulbIcon from 'assets/icons/bulb.svg';
+import logoutIcon from 'assets/icons/logout.svg';
+import penIcon from 'assets/icons/pen.svg';
+import plusIcon from 'assets/icons/plus.svg';
+import twitterIcon from 'assets/icons/twitter.svg';
+import ButtonIcon from './ButtonIcon';
+import { myTheme } from '../../../theme/mainTheme';
+
+const YellowBackground = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 240px;
+  width: 240px;
+  background: ${myTheme.primary};
+`;
+
+export default {
+  title: 'ButtonIcon',
+  component: ButtonIcon,
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={myTheme}>
+        <YellowBackground>
+          <Story />
+        </YellowBackground>
+      </ThemeProvider>
+    ),
+  ],
+} as Meta;
+
+export const Bulb: React.VFC<{}> = () => <ButtonIcon icon={bulbIcon} />;
+export const Logout: React.VFC<{}> = () => <ButtonIcon active icon={logoutIcon} />;
+export const Pen: React.VFC<{}> = () => <ButtonIcon icon={penIcon} />;
+export const Plus: React.VFC<{}> = () => <ButtonIcon icon={plusIcon} />;
+export const Twitter: React.VFC<{}> = () => <ButtonIcon icon={twitterIcon} />;
---
+++ src/components/atoms/ButtonIcon/ButtonIcon.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+type Props = {
+  icon?: string;
+  active?: boolean;
+};
+
+const ButtonIcon = styled.button<Props>`
+  width: 67px;
+  height: 67px;
+  border-radius: 20px;
+  background-image: url(${({ icon }) => icon});
+  background-size: 40%;
+  background-position: 50% 50%;
+  background-repeat: no-repeat;
+  border: none;
+
+  background-color: ${({ active }) => (active ? 'white' : 'transparent')};
+`;
+
+export default ButtonIcon;
